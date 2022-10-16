b01ab40e44d518f44e408c146b45d94d75c7abae
Marcos Amador <marcosamador8@hotmail.com>
2022-10-15 21:01:50 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-
 import "./app/style.css";
-import Router from "./app/Router";
-
+import  Router  from "./app/Router";
 
 const App = () => {
-    // const [productoName, setproductoName] = useState();
+
     return (
         <div>
             <Router />
         </div>
+        
     )
+
 }
 export default App
-
 
 
